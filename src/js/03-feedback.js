c41ae5fbe51b3fcf84fd3c726a48330014ef97ec
Olha Rakytianska <olya.rakytyanska@gmail.com>
2023-02-08 00:37:14 +0200

    Add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+currentFormData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+const formData = { ...currentFormData };
+
+formEl.addEventListener('input', throttle(onChangeData, 500));
+formEl.addEventListener('submit', onSubmit);
+onRefreshPage(currentFormData);
+
+function onChangeData(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(currentFormData);
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  event.currentTarget.reset();
+}
+
+function onRefreshPage({ email = '', message = '' }) {
+  formEl.email.value = email;
+  formEl.message.value = message;
+}
