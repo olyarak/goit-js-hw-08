3a9cb04514c3a6ef87e7b18a470fb356429c87db
Olha Rakytianska <olya.rakytyanska@gmail.com>
2023-02-07 22:05:36 +0200

    Add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryBox(items) {
+  return items.map(({ preview, original, description }) => {
+    const galleryItem = document.createElement('a');
+    galleryItem.classList.add('gallery__item');
+    galleryItem.href = original;
+
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = preview;
+    galleryImage.alt = description;
+
+    galleryItem.append(galleryImage);
+
+    return galleryItem;
+  });
+}
+
+const galleryContainerElements = createGalleryBox(galleryItems);
+galleryContainer.append(...galleryContainerElements);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
